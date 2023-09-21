6d50db3a8911eab012ad087d2c572acfb029b07a
unknown <binobajichiku@gmail.com>
2023-09-21 10:46:13 +0530

    added pdf

--- login-page/src/Login.jsx
+++ login-page/src/Login.jsx
@@ -55,6 +55,34 @@ const Login= () => {
 
     // decoded();
 
+    const downloadPDF = () => {
+      const userDetails = {
+        name: "John Doe",
+        fatherName: "Abcd",
+        dob: "January 1, 1990",
+        gender: "Male",
+        photo: "https://i.pinimg.com/originals/6b/7e/d6/6b7ed698713c09ad9e6afc7dcb996a09.jpg",
+        blood_group:"B+",
+        signature: "https://pbs.twimg.com/media/D9z0TuNU4AAp6HZ?format=jpg&name=4096x4096",
+        enrollment_no:"65451412154",
+      }
+      const pdf = new jsPDF("p", "mm", [110, 100]);
+    // const pdf=new jsPDF();
+    pdf.setFontSize(10);
+     let offsetY = 0;
+     let offsetX=0;
+     console.log();
+      pdf.addImage("https://upload.wikimedia.org/wikipedia/commons/f/fe/Seal_of_Odisha.png", "JPEG", 0, 5, 30, 30);
+      pdf.addImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOIe0E4d42KwXt6c_WZ8yFjyMOXDdrQ-gXbaidBzmQiQ&s","JPEG",33,5,60,30);
+      pdf.addImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKkUNBkPcoKNOTEradqz8QEFPhhC9afzf2Wa14gLSi&s","JPEG",55,45,40,40);
+      pdf.addImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyHcirjU8r73nJvIRVUO_gxXAsI1X-a9eYpQ4W5M32&s","JPEG",55,90,30,10);
+      pdf.text(5, 48, `Name: ${userDetails.name}`);
+      pdf.text(5, 56, `Father: ${userDetails.fatherName}`);
+      pdf.text(5, 64, `DOB: ${userDetails.dob}`);
+      pdf.text(5, 72, `Gender: ${userDetails.gender}`);
+      pdf.text(5, 80, `Blood Group: ${userDetails.blood_group}`);
+      pdf.text(5, 88, `Enrollment No: ${userDetails.enrollment_no}`);
+      pdf.save("user_details.pdf");}
 
   return (
     <div className="login-container">
@@ -64,8 +92,10 @@ const Login= () => {
       <h2>Login</h2>
       <input type="text" placeholder="Enter Uid" name="uid" onChange={updatedata} typeof='text' />
       <input type="password" placeholder="Enter Password" name="password" onChange={updatedata} typeof="password" />
-      <button type="submit"  disabled={data.password&&data.uid?false:true} onClick={check}>Login</button>
+      <button type="submit"  disabled={data.password&&data.uid?false:true} onClick={!login?check:""}>{login?"Login":"Logout"}</button>
     </div>
+   <>{login?<button onClick={downloadPDF} style={{width:"10%",backgroundColor:"white",height:"35px"}}>Download as PDF</button>:""}</>
+    
   </div>
 
   );
